d7c1e4b44002466b1f73036451ff88c2853f2fd2
diego <dgcuenca@gmail.com>
2023-03-29 05:52:47 -0500

    Add attributes to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_29_041914) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_29_105051) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,6 +37,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_29_041914) do
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "photo"
+    t.text "bio"
+    t.integer "posts_counter"
   end
 
   add_foreign_key "comments", "posts"
