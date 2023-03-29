b6190938dc7a452e125ece39d0e462192cdd6082
diego <dgcuenca@gmail.com>
2023-03-29 08:20:57 -0500

    Add foreign key with different name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_many :comments
     has_many :likes
+    has_many :posts, foreign_key: 'author_id'
     validates :photo, format: { with: /\Ahttps?:\/\/\S+\.(png|jpe?g|gif)\z/i, message: "must be a valid image URL" }
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_29_111501) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_29_131647) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_29_111501) do
   create_table "posts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -51,4 +53,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_29_111501) do
   add_foreign_key "comments", "users"
   add_foreign_key "likes", "posts"
   add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users", column: "author_id"
 end
