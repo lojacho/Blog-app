36be7c84ec81929fa4c1a30b3eb1f681c3149179
diego <dgcuenca@gmail.com>
2023-03-30 05:46:25 -0500

    Updates the comments counter for a post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,11 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
+
+  after_create_commit :update_post_comments_counter
+
+  def update_post_comments_counter
+    post.update(comments_counter: post.comments.count)
+  end
+
 end
