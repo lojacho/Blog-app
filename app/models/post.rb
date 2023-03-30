433c97d022ec594473cca3b673be2c0ea596c9da
diego <dgcuenca@gmail.com>
2023-03-30 05:30:07 -0500

    Updates the posts counter for a user

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,10 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
   has_many :likes
+
+  def update_user_posts_counter
+    author.posts_counter = author.posts.count
+    author.save
+  end
+  
 end
