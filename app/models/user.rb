369aae335e0430d025393e23a53c596b2ea6c47f
diego <dgcuenca@gmail.com>
2023-03-30 05:28:14 -0500

    Returns the 3 most recent posts for a given user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,9 @@ class User < ApplicationRecord
     with: %r{\Ahttps?://(?:www\.)?(?:unsplash\.com|.*unsplash\.com)/photos/.*(?:\.(?:png|jpe?g|gif))?\z}i,
     message: 'must be a valid image URL from Unsplash'
   }
+
+  def recent_posts
+    posts.order(created_at: :desc).limit(3)
+  end
   
 end
