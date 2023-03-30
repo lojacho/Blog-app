4acc7eb47c1240bd5fe6169e19b5948081615e23
diego <dgcuenca@gmail.com>
2023-03-30 04:24:17 -0500

    Change validation regexpress

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,9 @@ class User < ApplicationRecord
   has_many :comments
   has_many :likes
   has_many :posts, foreign_key: 'author_id'
-  validates :photo, format: { with: %r{\Ahttps?://\S+\.(png|jpe?g|gif)\z}i, message: 'must be a valid image URL' }
+  validates :photo, format: {
+    with: %r{\Ahttps?://(?:www\.)?(?:unsplash\.com|.*unsplash\.com)/photos/.*(?:\.(?:png|jpe?g|gif))?\z}i,
+    message: 'must be a valid image URL from Unsplash'
+  }
+  
 end
