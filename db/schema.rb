286b3d4a13f3cb2858762253b798655dec4a5730
diego <dgcuenca@gmail.com>
2023-03-31 10:49:04 -0500

    Add default value to likes counter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_31_154159) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_31_154815) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,7 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_31_154159) do
     t.string "title"
     t.text "text"
     t.integer "comments_counter", default: 0
-    t.integer "likes_counter"
+    t.integer "likes_counter", default: 0
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
