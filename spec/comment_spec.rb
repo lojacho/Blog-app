9f5ba39870b639b5dcfbda029f09c9b2d0f9305a
diego <dgcuenca@gmail.com>
2023-03-31 11:05:28 -0500

    Add test to count comments in a post

--- spec/comment_spec.rb
+++ spec/comment_spec.rb
@@ -7,16 +7,13 @@ require_relative '../app/models/user'
 RSpec.describe Comment, type: :model do
     let(:first_user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
     let(:first_post) { Post.create(author: first_user, title: 'Hello', text: 'This is my first post') }
+    subject { first_post.comments.create(post: first_post, user_id: first_user.id, text: 'This is my first comment!', author_id: first_user.id, author_type: 'User') }
   
     describe '#update_post_comments_counter' do
-      it 'updates the comments counter of the associated post' do
-        first_post.save
-  
-        subject { first_post.comments.create(user: first_user, text: 'This is my first comment!', author: first_user) }
-  
+      it 'updates the comments counter of the associated post' do  
         expect {
           subject.update_post_comments_counter
         }.to change { first_post.comments_counter }.from(0).to(1)
       end
     end
-  end
+end
