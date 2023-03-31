d56d6d61545e0a6463f4979974a224726063a553
diego <dgcuenca@gmail.com>
2023-03-30 19:46:49 -0500

    Correct errors after implementations of CR

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_29_132828) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_31_002432) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_29_132828) do
     t.bigint "user_id", null: false
     t.bigint "post_id", null: false
     t.text "text"
+    t.string "author_type", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_type", "author_id"], name: "index_comments_on_author"
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
