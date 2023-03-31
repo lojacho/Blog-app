741a100b888d41e249fc104cc9a0248907af3587
diego <dgcuenca@gmail.com>
2023-03-31 11:16:58 -0500

    Test number of likes in a post

---
+++ spec/like_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+require_relative '../app/models/post'
+require_relative '../app/models/user'
+require_relative '../app/models/like'
+
+
+RSpec.describe Like, type: :model do
+    let(:first_user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+    let(:first_post) { Post.create(author: first_user, title: 'Hello', text: 'This is my first post') }
+    
+    subject { first_post.likes.create(user: first_user, post: first_post) }
+  
+    describe '#update_likes_comments_counter' do
+      it 'updates the likes counter of the associated comment' do  
+        expect {
+          subject.update_post_likes_counter
+        }.to change { first_post.likes_counter }.from(0).to(1)
+      end
+    end
+end
