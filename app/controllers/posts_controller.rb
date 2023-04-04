1c269fe08cba7bb8fb599d9da6e60b8dc87da742
diego <dgcuenca@gmail.com>
2023-04-04 10:49:24 -0500

    Define index and show actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,9 @@
 class PostsController < ApplicationController
+      # Renders the default view for the index action.
+  def index
+  end
+
+  # Renders the default view for the show action.
+  def show
+  end
 end
