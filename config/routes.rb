a953f398a59a5a9105482b5ba4555b2cdc9340fe
diego <dgcuenca@gmail.com>
2023-04-07 14:57:35 -0500

    Create routes to handling a form to create a post

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 root "users#index"
 
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+    resources :posts, only: [:index, :show, :new, :create]
   end
   
 end
