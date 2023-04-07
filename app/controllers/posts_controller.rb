a136019295dc8ae0b161bf132beb3829bbab8c18
diego <dgcuenca@gmail.com>
2023-04-06 22:34:47 -0500

    Add link to posts view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,8 @@
 class PostsController < ApplicationController
   # Renders the default view for the index action.
-  def index; end
+  def index
+    @user = User.find(request.params['user_id'])
+  end
 
   # Renders the default view for the show action.
   def show; end
