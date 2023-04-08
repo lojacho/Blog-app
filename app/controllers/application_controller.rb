10b9b95086d3ba691e9c3f462418bd63c041f664
diego <dgcuenca@gmail.com>
2023-04-08 11:28:18 -0500

    Add form to enter new post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
     def current_user
-        User.first
+      @current_user ||= User.first
     end
-end
+  end
+  
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,10 +10,19 @@ class PostsController < ApplicationController
   end
 
   def new
+    @current_user = current_user
     @post = Post.new
   end
 
   def create
+    @current_user = current_user
     @post = @current_user.posts.new(post_params)
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+
 end
