ab61191f07847a4712f2b289abeade0618c30e8b
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-04-14 10:14:20 -0500

    Add route to delete comments.

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ root "users#index"
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create, :destroy] do
-      resources :comments, only: [:new, :create]
+      resources :comments, only: [:new, :create, :destroy]
       resources :likes, only: [:create]
     end
   end
