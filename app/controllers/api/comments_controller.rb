c1c6861b02ad268060f04749eeb0f92459f8d165
Paul Sáenz Sucre <paulsaenzsucre@outlook.com>
2023-04-14 17:15:03 -0500

    Add api endpoint to crate comment.

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -3,4 +3,21 @@ class Api::CommentsController < ApplicationController
     @post = Post.find(request.params['post_id'])
     render :json => @post.comments.all
   end
+
+  def create
+    comment_params = params.require(:comment).permit(:user_id, :post_id, :text)
+    # Aquí puedes usar los valores de name y email
+
+    @user = User.find(comment_params[:user_id])
+    @post = Post.find(comment_params[:post_id])
+    @comment = @post.comments.create(comment_params[:text])
+    @comment.author = @user
+    @comment.user_id = @user.id
+    @comment.author_type = 'User'
+    if @comment.save
+      render :json => {"status": 'Siuuuuuuuuuuuu'}
+    else
+      render :json => {"status": 'Nou'}
+    end
+  end
 end
